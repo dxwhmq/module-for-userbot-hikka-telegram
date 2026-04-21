c00297c5dcb3368de5519487ef9a2b9fa720d26f
dxwhmq <dxwhmq@gmail.com>
2026-04-21 07:22:42 +0700

    Update dxhelp.py

--- dxhelp.py
+++ dxhelp.py
@@ -1,30 +1,26 @@
-# meta developer: @dxwhmq
+ # meta developer: @dxwhmq
 # scope: hikka_only
 
 from .. import loader, utils
 
 @loader.tds
 class dxwhmqHelpMod(loader.Module):
-    """Стильный хелп в формате цитаты (blockquote)"""
+    """Стильный хелп в формате цитаты"""
     strings = {"name": "dxwhmqHelp"}
 
+    @loader.command()
     async def dxhelp(self, message):
         """Выводит список всех модулей в формате blockquote"""
-        all_modules = self.all_modules
+        all_modules = self.lookup("Loader").modules # Достаем список через загрузчик
         out = "✧ dxwhmq_systems ✧\n\n"
         
         mods = []
         for mod in all_modules:
-            try:
-                # Берем имя модуля из его строк
-                name = mod.strings.get('name', mod.__class__.__name__)
-                mods.append(f"▫️ {name}")
-            except:
-                continue
+            name = getattr(mod, "strings", {}).get("name", mod.__class__.__name__)
+            mods.append(f"▫️ {name}")
         
         out += "\n".join(sorted(mods))
         
-        # Оборачиваем в blockquote для эффекта цитаты
-        # Используем HTML парс-мод, чтобы телега поняла тег
+        # Оборачиваем в blockquote (цитату)
         await utils.answer(message, f"<blockquote>{out}</blockquote>")
         
